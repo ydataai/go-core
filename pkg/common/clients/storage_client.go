adf17b975eb7a29e02d38c795de6cfd64043ac0b
wsalles <wallace_robinson@hotmail.com>
2021-11-16 20:36:00 -0300

    chore: revert path suffix as parameter

--- pkg/common/clients/storage_client.go
+++ pkg/common/clients/storage_client.go
@@ -10,7 +10,6 @@ import (
 // StorageClient is a structure that holds all the dependencies for the following client
 type StorageClient struct {
 	logger        logging.Logger
-	pathSufix     string
 	configuration StorageClientConfiguration
 }
 
@@ -22,17 +21,16 @@ type StorageClientInterface interface {
 }
 
 // NewStorageClient returns an initialized struct with the required dependencies injected
-func NewStorageClient(logger logging.Logger, pathSufix string, configuration StorageClientConfiguration) StorageClient {
+func NewStorageClient(logger logging.Logger, configuration StorageClientConfiguration) StorageClient {
 	return StorageClient{
 		logger:        logger,
-		pathSufix:     pathSufix,
 		configuration: configuration,
 	}
 }
 
 // CreateDirectory attempts to create a directory to hold requirements.txt
 func (sc *StorageClient) CreateDirectory(relativePath string) error {
-	fullPath := fmt.Sprintf("%s/%s/%s", sc.configuration.BasePath, sc.pathSufix, relativePath)
+	fullPath := fmt.Sprintf("%s/%s", sc.configuration.BasePath, relativePath)
 
 	sc.logger.Info("Attempting to create directory ", fullPath)
 
@@ -45,14 +43,14 @@ func (sc *StorageClient) CreateDirectory(relativePath string) error {
 
 // RemoveDirectory attempts to remove the directory that holds requirements.txt
 func (sc *StorageClient) RemoveDirectory(relativePath string) error {
-	fullPath := fmt.Sprintf("%s/%s/%s", sc.configuration.BasePath, sc.pathSufix, relativePath)
+	fullPath := fmt.Sprintf("%s/%s", sc.configuration.BasePath, relativePath)
 	sc.logger.Info("Attempting to remove ", fullPath)
 	return os.RemoveAll(fullPath)
 }
 
 // CheckIfExists attempts to check if the directory exists
 func (sc *StorageClient) CheckIfExists(relativePath string) bool {
-	fullPath := fmt.Sprintf("%s/%s/%s", sc.configuration.BasePath, sc.pathSufix, relativePath)
+	fullPath := fmt.Sprintf("%s/%s", sc.configuration.BasePath, relativePath)
 
 	_, err := os.Stat(fullPath)
 	if err != nil && os.IsNotExist(err) {
