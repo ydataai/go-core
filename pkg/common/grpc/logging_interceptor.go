4a14723bd3c4ab2056843fe52f39638c7bc664fd
Renato Guimaraes <renatoaguimaraes@gmail.com>
2021-12-21 12:23:12 -0300

    loggging format

--- pkg/common/grpc/logging_interceptor.go
+++ pkg/common/grpc/logging_interceptor.go
@@ -10,12 +10,12 @@ import (
 // LoggingUnaryServerInterceptor returns a new unary server interceptors that logs the payloads of requests.
 func LoggingUnaryServerInterceptor(logger logging.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		logger.Infof("gRPC req %v -> %v", info.FullMethod, req)
+		logger.Infof("gRPC req %s -> %v", info.FullMethod, req)
 		resp, err := handler(ctx, req)
 		if err == nil {
-			logger.Errorf("gRPC err %v -> %v", info.FullMethod, err)
+			logger.Errorf("gRPC err %s -> %v", info.FullMethod, err)
 		}
-		logger.Infof("gRPC res %v -> %v", info.FullMethod, resp)
+		logger.Infof("gRPC res %s -> %v", info.FullMethod, resp)
 		return resp, err
 	}
 }
