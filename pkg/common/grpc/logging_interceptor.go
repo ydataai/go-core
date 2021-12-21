a851cdc358dc8dd8999ea398e8258476946944e3
Renato Guimaraes <renatoaguimaraes@gmail.com>
2021-12-21 12:23:45 -0300

    refactoring

--- pkg/common/grpc/logging_interceptor.go
+++ pkg/common/grpc/logging_interceptor.go
@@ -11,11 +11,11 @@ import (
 func LoggingUnaryServerInterceptor(logger logging.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		logger.Infof("gRPC req %s -> %v", info.FullMethod, req)
-		resp, err := handler(ctx, req)
+		res, err := handler(ctx, req)
 		if err == nil {
 			logger.Errorf("gRPC err %s -> %v", info.FullMethod, err)
 		}
-		logger.Infof("gRPC res %s -> %v", info.FullMethod, resp)
-		return resp, err
+		logger.Infof("gRPC res %s -> %v", info.FullMethod, res)
+		return res, err
 	}
 }
