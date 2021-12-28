e01537cf2ee4f7dd772b866222062d279866beb3
Renato Guimaraes <renatoaguimaraes@gmail.com>
2021-12-28 10:37:12 -0300

    spaces between square brackets

--- pkg/common/grpc/logging_interceptor.go
+++ pkg/common/grpc/logging_interceptor.go
@@ -10,12 +10,12 @@ import (
 // LoggingUnaryServerInterceptor returns a new unary server interceptors that logs the payloads of requests.
 func LoggingUnaryServerInterceptor(logger logging.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		logger.Infof("gRPC req %s -> [%v]", info.FullMethod, req)
+		logger.Infof("gRPC req %s -> [ %v ]", info.FullMethod, req)
 		res, err := handler(ctx, req)
 		if err != nil {
-			logger.Errorf("gRPC err %s -> [%v]", info.FullMethod, err)
+			logger.Errorf("gRPC err %s -> [ %v ]", info.FullMethod, err)
 		}
-		logger.Infof("gRPC res %s -> [%v]", info.FullMethod, res)
+		logger.Infof("gRPC res %s -> [ %v ]", info.FullMethod, res)
 		return res, err
 	}
 }
