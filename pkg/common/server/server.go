78259b76ef8c804697491be655ad2815c3bda015
wsalles <wallace_robinson@hotmail.com>
2022-01-10 17:01:48 -0300

    include callback for when the server is ready + fix bugs
    closes [PLT-134]

--- pkg/common/server/server.go
+++ pkg/common/server/server.go
@@ -3,10 +3,13 @@ package server
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ydataai/go-core/pkg/common/logging"
@@ -50,19 +53,9 @@ func (s *Server) Run(ctx context.Context) {
 		if err := s.httpServer.ListenAndServe(); err != http.ErrServerClosed {
 			s.logger.Errorf("unexpected error while running server %v", err)
 		}
-
-		c := make(chan os.Signal, 3)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		<-c
-
-		s.logger.Infof("Shutdown Server ...")
-
-		if err := s.httpServer.Shutdown(ctx); err != nil {
-			s.logger.Fatal("Server forced to shutdown: ", err)
-		}
-
-		s.logger.Infof("Server exiting")
 	}()
+
+	go s.shutdown(ctx)
 }
 
 // RunSecurely when called starts the https server
@@ -75,19 +68,9 @@ func (s *Server) RunSecurely(ctx context.Context) {
 		if err := s.httpServer.ListenAndServeTLS(s.configuration.CertificateFile, s.configuration.CertificateKeyFile); err != http.ErrServerClosed {
 			s.logger.Errorf("unexpected error while running server %v", err.Error())
 		}
-
-		c := make(chan os.Signal, 2)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		<-c
-
-		s.logger.Infof("Shutdown Server ...")
-
-		if err := s.httpServer.Shutdown(ctx); err != nil {
-			s.logger.Fatalf("Server forced to shutdown: %v", err)
-		}
-
-		s.logger.Infof("Server exiting")
 	}()
+
+	go s.shutdown(ctx)
 }
 
 // AddHealthz creates a route to LivenessProbe
@@ -107,9 +90,40 @@ func (s *Server) AddReadyz(status func() bool, urls ...string) {
 	s.Router.GET(url, s.readyz())
 }
 
+// WaitingToBeReady executes a callback when the server is ready.
+func (s *Server) WaitingToBeReady(callback func()) {
+	for {
+		conn, err := net.DialTimeout("tcp", net.JoinHostPort(s.configuration.Host, strconv.Itoa(s.configuration.Port)), s.configuration.RequestTimeout)
+		if err != nil {
+			s.logger.Debug("Server is not ready yet!")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		if conn != nil {
+			s.logger.Infof("Server is ready!")
+			break
+		}
+	}
+	callback()
+}
+
 func (s *Server) httpServerSetup() {
 	s.httpServer = &http.Server{
 		Addr:    fmt.Sprintf("%s:%d", s.configuration.Host, s.configuration.Port),
 		Handler: s.Router,
 	}
+}
+
+func (s *Server) shutdown(ctx context.Context) {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	s.logger.Infof("Shutdown Server ...")
+
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		s.logger.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	s.logger.Infof("Server exiting")
 }
