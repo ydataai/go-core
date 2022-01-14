8ab490b2b1635552af6c038dc9fdb28a2c08590e
Wallace Salles <wallace.salles@ydata.ai>
2022-01-14 16:17:15 -0300

    fix(redis-client) make sure the server is ready to write (#52)

--- pkg/common/clients/redis_client.go
+++ pkg/common/clients/redis_client.go
@@ -2,6 +2,7 @@ package clients
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/ydataai/go-core/pkg/common/logging"
@@ -14,12 +15,18 @@ type RedisClient struct {
 
 // NewRedisClient creates a new RedisClient (redis.Client) instance.
 func NewRedisClient(config RedisConfiguration, logger logging.Logger) RedisClient {
+	ctx := context.Background()
 	client := redis.NewClient(&redis.Options{
 		Addr: config.Address,
 	})
-	err := client.Ping(context.Background()).Err()
+	err := client.Ping(ctx).Err()
 	if err != nil {
 		logger.Fatalf("Error while connect to Redis: %s", config.Address)
+	}
+	// make sure the redis server is ready to write.
+	err = client.Set(ctx, "lastUpdate", time.Now(), time.Minute).Err()
+	if err != nil {
+		logger.Fatalf("Redis Server is ready-only. Err: %v", err)
 	}
 	return RedisClient{client}
 }
