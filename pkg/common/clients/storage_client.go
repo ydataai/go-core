8dfba5c4d004d21641b6f534e545de166c2cd99a
Renato Guimaraes <renatoaguimaraes@gmail.com>
2022-09-12 15:32:23 -0300

    fix(storage-client): Path Rename and BasePath

--- pkg/common/clients/storage_client.go
+++ pkg/common/clients/storage_client.go
@@ -15,9 +15,11 @@ type StorageClient struct {
 
 // StorageClientInterface defines storage client interface
 type StorageClientInterface interface {
+	BasePath() string
 	CreateDirectory(relativePath string) error
 	RemoveDirectory(relativePath string) error
 	CheckIfExists(relativePath string) bool
+	Rename(fromRelativePath, toAbsolutePath string) bool
 }
 
 // NewStorageClient returns an initialized struct with the required dependencies injected
@@ -28,13 +30,19 @@ func NewStorageClient(logger logging.Logger, configuration StorageClientConfigur
 	}
 }
 
+// BasePath returns a configured base path
+func (sc *StorageClient) BasePath() string {
+	return sc.configuration.BasePath
+}
+
 // CreateDirectory attempts to create a directory to hold requirements.txt
 func (sc *StorageClient) CreateDirectory(relativePath string) error {
 	fullPath := fmt.Sprintf("%s/%s", sc.configuration.BasePath, relativePath)
 
-	sc.logger.Info("Attempting to create directory ", fullPath)
+	sc.logger.Infof("attempting to create directory %s", fullPath)
 
 	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		sc.logger.Errorf("while create path %s", relativePath)
 		return os.MkdirAll(fullPath, os.ModePerm)
 	}
 
@@ -44,16 +52,30 @@ func (sc *StorageClient) CreateDirectory(relativePath string) error {
 // RemoveDirectory attempts to remove the directory that holds requirements.txt
 func (sc *StorageClient) RemoveDirectory(relativePath string) error {
 	fullPath := fmt.Sprintf("%s/%s", sc.configuration.BasePath, relativePath)
-	sc.logger.Info("Attempting to remove ", fullPath)
-	return os.RemoveAll(fullPath)
+	sc.logger.Infof("attempting to remove %s", fullPath)
+	if err := os.RemoveAll(fullPath); err != nil {
+		sc.logger.Errorf("while remove path %s", relativePath)
+		return err
+	}
+	return nil
 }
 
 // CheckIfExists attempts to check if the directory exists
 func (sc *StorageClient) CheckIfExists(relativePath string) bool {
 	fullPath := fmt.Sprintf("%s/%s", sc.configuration.BasePath, relativePath)
+	if _, err := os.Stat(fullPath); err != nil && os.IsNotExist(err) {
+		sc.logger.Errorf("while check path %s", relativePath)
+		return false
+	}
 
-	_, err := os.Stat(fullPath)
-	if err != nil && os.IsNotExist(err) {
+	return true
+}
+
+// Rename attempts to rename a path
+func (sc *StorageClient) Rename(fromRelativePath, toAbsolutePath string) bool {
+	sc.logger.Infof("attempting to rename from %s to %s", fromRelativePath, toAbsolutePath)
+	if err := os.Rename(fromRelativePath, toAbsolutePath); err != nil {
+		sc.logger.Errorf("while rename path from %s to %s", fromRelativePath, toAbsolutePath)
 		return false
 	}
 
