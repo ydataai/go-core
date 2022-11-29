5431fe59e1729d4522df16f9ac0b7941aeffbb5c
Renato Guimaraes <renatoaguimaraes@gmail.com>
2022-11-29 15:18:10 -0300

    doc

--- pkg/common/collections/slices.go
+++ pkg/common/collections/slices.go
@@ -1,7 +1,6 @@
 package collections
 
-// TODO move to go-core
-
+// Filter an slice given a filter function
 func Filter[T interface{}](list []T, test func(T) bool) []T {
 	ret := []T{}
 	for _, item := range list {
@@ -12,6 +11,7 @@ func Filter[T interface{}](list []T, test func(T) bool) []T {
 	return ret
 }
 
+// Map convert a list given a map function
 func Map[S, D interface{}](list []S, mapTo func(S) D) []D {
 	ret := []D{}
 	for _, item := range list {
@@ -20,6 +20,7 @@ func Map[S, D interface{}](list []S, mapTo func(S) D) []D {
 	return ret
 }
 
+// FilterAndMap filter a list and converts to other list given a filter and a map functions
 func FilterAndMap[S, D interface{}](list []S, test func(S) bool, mapTo func(S) D) []D {
 	ret := []D{}
 	for _, item := range list {
