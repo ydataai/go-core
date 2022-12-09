a646881396380cfa918e9e2c8c4dd164e149dc9b
Renato Guimarães <renatoaguimaraes@gmail.com>
2022-12-09 00:04:26 -0300

    fix(k8s): rest client config with rate limit (#104)

---
+++ pkg/services/k8s/client_config.go
@@ -0,0 +1,27 @@
+// Package k8s is an util library to deal with kubernetes.
+package k8s
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/flowcontrol"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+// RestClientConfiguration defines required variables to configure the environment
+type RestClientConfiguration struct {
+	ClientQPS   float32 `envconfig:"K8S_REST_CLIENT_QPS" default:"100"`
+	ClientBurst int     `envconfig:"K8S_REST_CLIENT_BURST" default:"500"`
+}
+
+// LoadFromEnvVars for RestClientConfiguration.
+func (c *RestClientConfiguration) LoadFromEnvVars() error {
+	return envconfig.Process("", c)
+}
+
+// Config creates rest client configuration with TokenBucketRateLimiter.
+func Config(config RestClientConfiguration) *rest.Config {
+	kconfig := ctrl.GetConfigOrDie()
+	kconfig.RateLimiter = flowcontrol.NewTokenBucketRateLimiter(config.ClientQPS, config.ClientBurst)
+	return kconfig
+}
