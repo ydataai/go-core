1b2f4f669419fa74295167f4e1d873d87e5a1d1e
Luís Portela Afonso <luis.portela@ydata.ai>
2023-03-28 20:16:46 +0100

    fix(server): slightly improvements to HTTP Server (#129)

--- pkg/common/server/server_handler.go
+++ pkg/common/server/server_handler.go
@@ -7,14 +7,14 @@ import (
 )
 
 // healthz is a liveness probe
-func (s *Server) healthz() gin.HandlerFunc {
+func (s *server) healthz() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Status(http.StatusNoContent)
 	}
 }
 
 // readyz is a readiness probe
-func (s *Server) readyz() gin.HandlerFunc {
+func (s *server) readyz() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if !s.readyzFunc() {
 			http.Error(ctx.Writer, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
